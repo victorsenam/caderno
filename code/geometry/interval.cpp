f199fc29ad100e25cd066e8bcf1db90670c8aa2c
Victor Sena Molero <victorsenam@gmail.com>
2017-01-24 23:15:32 -0200

    Reescrevendo vector e interval

--- code/geometry/interval.cpp
+++ code/geometry/interval.cpp
@@ -1,21 +1,22 @@
 #include "header.cpp"
+#define ite ite
 
-template<typename cood> struct interval {
+template<typename cood> struct inte {
     cood a,b;
 
-    interval<cood> () {}
-    interval<cood> (cood s, cood t) : a(s), b(t)
+    ite () {}
+    ite (cood s, cood t) : a(s), b(t)
     { if (a > b) swap(a,b); }
-    interval<cood> (cood x) : a(x), b(x) {}
+    explicit ite (cood x) : a(x), b(x) {}
 
     // XXX assumes a <= b
     inline cood size () const
     { return b - a; }
 
     // XXX assumes a <= b
-    inline bool contains (const interval & ot, cood eps = 0) const
+    inline bool contains (ite ot, cood eps = 0) const
     { return lte(a,ot.a,eps) && lte(ot.b,b,eps);}
     // XXX assumes a <= b
-    inline bool intersects (const interval & ot, cood eps = 0) const
-    { return contains(interval<cood>(ot.a), eps) || contains(interval<cood>(ot.b), eps) || ot.contains(*this, eps); }
+    inline bool intersects (ite ot, cood eps = 0) const
+    { return contains(ite(ot.a), eps) || contains(ite(ot.b), eps) || ot.contains(*this, eps); }
 };
---
+++ code/geometry/vector.cpp
@@ -0,0 +1,70 @@
+#include "header.cpp"
+#define vec vect<cood>
+
+template<typename cood> struct vect {
+    cood x,y;
+    
+    // constructors
+    vec () {}
+    vec (cood a, cood b) : x(a), y(b) {}
+
+    // basics
+    inline bool operator < (vec ot) const // lex compare
+    { return (x < ot.x || (x == ot.x && y < ot.y)); }
+
+    // transforming
+    inline vec operator - (vec ot) const
+    { return vec(x - ot.x, y - ot.y); }
+    inline vec operator + (vec ot) const
+    { return vec(x + ot.x, y + ot.y); }
+    inline vec operator * (cood a) const
+    { return vec(x * a, y * a); }
+    inline vec operator / (cood a) const
+    { return vec(x / a, y / a); }
+    inline vect<double> rotate (double ang) const // counter-clockwise
+    { return vect<double>(cos(ang) * x + sin(ang) * y, - sin(ang) * x + cos(ang) * y); }
+
+    // math
+    inline cood operator ^ (vec ot) const // cross
+    { return x * ot.y - y * ot.x; }
+    inline cood operator * (vec ot) const // inner
+    { return x * ot.x + y * ot.y; }
+    inline cood sq (vec ot = vec(0,0)) const // squared 2-norm (and distance)
+    { return ((*this)-ot)*((*this)-ot); }
+    inline double nr (vec ot = vec(0,0)) const // 2-norm (and distance)
+    { return sqrt(sq(ot)); }
+    inline cood ar (vec a, vec b) const // oriented area (positive if b is to the left of a)
+    { return (a-(*this))^(b-(*this)); }
+
+    // XXX: not a full comparsion operator, use compare for ordering
+    // counter-clockwise comparsion (to the left means greater)
+    // returns -1 if a < b [a->b is counter-clockwise]
+    // returns 0 if a == b [colinear]
+    // returns 1 if a > b [a->b is clockwise]
+    int left (vec a, vec b, cood eps = 0) const
+    { cood o = area(a, b); return (o > -eps) - (o < eps); }
+
+    // divide the plane by anc
+    // returns 0 if the counter-clockwise angle between anc and this is x s.t. 0 <= x < pi
+    // returns 1 otherwise (pi <= x < 2pi)
+    inline bool halfplane (vec anc = vec(1,0), cood eps = 0) const {
+        int o = vec(0,0).left(anc, *this, eps);
+        if (o == 0)
+            return (*this)*(anc) < 0;
+        return (o == 1);
+    }
+
+    // full comparsion
+    // orders by clockwise order starting from the (0,-1) direction (upwards)
+    // settles draws by proximity to this point
+    bool compare (vec ot, vec anc = (1,0), cood eps = 0) const {
+        bool s[2] = {halfplane(anc, eps), ot.halfplane(anc, eps)};
+        if (s[0] != s[1])
+            return s[0] < s[1];
+
+        int pr = vec(0,0).left((*this), ot, eps);
+        if (pr)
+            return (pr < 0);
+        return sq(a) < sq(b);
+    }
+};
