8991093cafa789fbe7c27e8d0a41137626258e02
Yan Couto <yancouto@gmail.com>
2017-05-23 16:12:36 -0300

    Reformatting

--- code/geometry.cpp
+++ code/geometry.cpp
@@ -16,88 +16,88 @@ typedef pair<ll,ll> pii;
 const double pi = acos(-1.);
 
 struct vec { // vector
-    // === BASIC ===
-    cood x, y;
-    vec () : x(0), y(0) {}
-    vec (cood a, cood b) : x(a), y(b) {}
-    friend ostream& operator<<(ostream& os, vec o);
+	// === BASIC ===
+	cood x, y;
+	vec () : x(0), y(0) {}
+	vec (cood a, cood b) : x(a), y(b) {}
+	friend ostream& operator<<(ostream& os, vec o);
 
-    vec operator - (vec o)
-    { return vec(x - o.x, y - o.y); }
-    vec operator + (vec o)
-    { return vec(x + o.x, y + o.y); }
-    vec operator * (cood o)
-    { return vec(x * o, y * o); }
-    vec operator / (cood o)
-    { return vec(x / o, y / o); }
-    cood operator ^ (vec o)
-    { return x * o.y - y * o.x; }
-    cood operator * (vec o) 
-    { return x * o.x + y * o.y; }
+	vec operator - (vec o)
+	{ return vec(x - o.x, y - o.y); }
+	vec operator + (vec o)
+	{ return vec(x + o.x, y + o.y); }
+	vec operator * (cood o)
+	{ return vec(x * o, y * o); }
+	vec operator / (cood o)
+	{ return vec(x / o, y / o); }
+	cood operator ^ (vec o)
+	{ return x * o.y - y * o.x; }
+	cood operator * (vec o)
+	{ return x * o.x + y * o.y; }
 
-    cood sq (vec o = vec())
-    { return ((*this)-o)*((*this)-o); }
-    double nr (vec o = vec())
-    { return sqrt(sq(o)); }
+	cood sq (vec o = vec())
+	{ return ((*this)-o)*((*this)-o); }
+	double nr (vec o = vec())
+	{ return sqrt(sq(o)); }
 
-    cood ar (vec a, vec b) // ccw signed area (positive if this is to the left of ab)
-    { return (b-a)^((*this)-a); }
-    int sd (vec a, vec b) // which side is this from ab? (-1 left, 0 over, 1 right)
-    { cood o = ar(a,b); return (o < -eps) - (o > eps); }
+	cood ar (vec a, vec b) // ccw signed area (positive if this is to the left of ab)
+	{ return (b - a) ^ ((*this) - a); }
+	int sd (vec a, vec b) // which side is this from ab? (-1 left, 0 over, 1 right)
+	{ cood o = ar(a, b); return (o < -eps) - (o > eps); }
 
-    // === ADVANCED ===
-    // rotate ccw by a (fails with ll)
-    vec rotate (double a)
-    { return vec(cos(a) * x - sin(a) * y, sin(a) * x + cos(a) * y); }
+	// === ADVANCED ===
+	// rotate ccw by a (fails with ll)
+	vec rotate (double a)
+	{ return vec(cos(a) * x - sin(a) * y, sin(a) * x + cos(a) * y); }
 
-    // divide the plane relative to anc
-    // 0 if the ccw angle from anc to this is in [0,pi) and 1 otherwise, origin goes to 0
-    bool halfplane (vec anc = vec(1,0)) {
-        cood l = sd(vec(),anc);
-        if (l == 0)
-            return (x < -eps);
-        return (l == 1);
-    }
+	// divide the plane relative to anc
+	// 0 if the ccw angle from anc to this is in [0,pi) and 1 otherwise, origin goes to 0
+	bool halfplane (vec anc = vec(1,0)) {
+		cood l = sd(vec(), anc);
+		if (l == 0)
+			return (x < -eps);
+		return (l == 1);
+	}
 
-    // ordering (ccw angle from anc, distance to origin)
-    // is this < o?
-    // PRECISION : ok with double if norm in [-1e4,1e3]
-    bool compare (vec o, vec anc = vec(1,0)) {
-        bool s[2] = {halfplane(anc), o.halfplane(anc)};
-        if (s[0] != s[1])
-            return s[0] < s[1];
+	// ordering (ccw angle from anc, distance to origin)
+	// is this < o?
+	// PRECISION : ok with double if norm in [-1e4,1e3]
+	bool compare (vec o, vec anc = vec(1,0)) {
+		bool s[2] = {halfplane(anc), o.halfplane(anc)};
+		if (s[0] != s[1])
+			return s[0] < s[1];
 
-        cood l = sd(o,vec());
-        if (l == 0)
-            return sq() < o.sq() - eps;
-        return (l == -1);
-    }
+		cood l = sd(o, vec());
+		if (l == 0)
+			return sq() < o.sq() - eps;
+		return (l == -1);
+	}
 
-    // tests TODO
-    // is this inside segment st? (tip of segment included, change for -eps otherwise)
-    bool in_seg (vec s, vec t)
-    { return (sd(s,t) == 0) && ((*this)-s)*((*this)-t) < eps; }
+	// tests TODO
+	// is this inside segment st? (tip of segment included, change for -eps otherwise)
+	bool in_seg (vec s, vec t)
+	{ return (sd(s, t) == 0) && ((*this) - s) * ((*this) - t) < eps; }
 };
 ostream& operator<<(ostream& os, vec o)
 { return os << '(' << o.x << ", " << o.y << ')'; }
 
 // tests TODO
 struct lin { // line
-    cood a, b, c;
+	cood a, b, c;
 
-    lin () {}
-    lin (cood x, cood y, cood z) : a(x), b(y), c(z) {}
-    lin (vec s, vec t) : a(t.y - s.y), b(s.x - t.x), c(a*s.x + b*s.y) {}
+	lin () {}
+	lin (cood x, cood y, cood z) : a(x), b(y), c(z) {}
+	lin (vec s, vec t) : a(t.y - s.y), b(s.x - t.x), c(a * s.x + b * s.y) {}
 
-    // parallel to this through p
-    lin move (vec p) { return lin(a, b, a*p.x + b*p.y); }
+	// parallel to this through p
+	lin parll (vec p) { return lin(a, b, a * p.x + b * p.y); }
 
-    // line intersection
-    vec inter (lin o) {
-        cood d = a*o.b - o.a*b;
-        if (abs(d) < eps) throw 0; // parallel
-        return vec((o.b*c - b*o.c)/d, (a*o.c - o.a*c)/d);
-    }
+	// line intersection
+	vec inter (lin o) {
+		cood d = a * o.b - o.a * b;
+		if (abs(d) < eps) throw 0; // parallel
+		return vec((o.b * c - b * o.c) / d, (a * o.c - o.a * c) / d);
+	}
 };
 
 // tests TODO
--- code/math/fft.cpp
+++ code/math/fft.cpp
@@ -12,54 +12,54 @@ typedef complex<double> cpx;
 // XXX assumes v.size() is power of 2
 // if inv is true, runs inverse DFT
 void fft (vector<cpx> & ans, vector<cpx> & a, vector<int> & p, bool inv) {
-    int n = a.size();
-    assert(!(n&(n-1)));
-    assert(n == int(ans.size()));
-    assert(n == int(p.size()));
+	int n = a.size();
+	assert(!(n&(n-1)));
+	assert(n == int(ans.size()));
+	assert(n == int(p.size()));
 
-    for (int i = 0; i < n; i++)
-        ans[i] = a[p[i]];
+	for (int i = 0; i < n; i++)
+		ans[i] = a[p[i]];
 
-    for (int sz = 1; sz < n; sz <<= 1) {
-        const cpx root(cos(pi / sz), sin((1. - inv - inv) * pi / sz));
-        for (int j = 0; j < n; j += (sz << 1)) {
-            cpx w = 1;
-            for (int i = 0; i < sz; i++) {
-                const cpx t = w * ans[i + j + sz];
-                ans[i + j + sz] = ans[i + j] - t;
-                ans[i + j] += t;
-                w *= root;
-            }
-        }
-    }
+	for (int sz = 1; sz < n; sz <<= 1) {
+		const cpx root(cos(pi / sz), sin((1. - inv - inv) * pi / sz));
+		for (int j = 0; j < n; j += (sz << 1)) {
+			cpx w = 1;
+			for (int i = 0; i < sz; i++) {
+				const cpx t = w * ans[i + j + sz];
+				ans[i + j + sz] = ans[i + j] - t;
+				ans[i + j] += t;
+				w *= root;
+			}
+		}
+	}
 
-    if (inv)
-        for (int i = 0; i < n; i++)
-            ans[i] /= n;
+	if (inv)
+		for (int i = 0; i < n; i++)
+			ans[i] /= n;
 }
 
 // XXX tested precision: 9 decimal digits (1e-9 when values vary on [-10,10])
 void product (vector<cpx> & ans, vector<cpx> & a, vector<cpx> & b, bool padded = 0) {
-    int n = max(a.size(), b.size());
-    
-    if (!padded) {
-        while (n&(n-1)) n += (n&-n); // geq power of 2
-        n += n;
+	int n = max(a.size(), b.size());
 
-        a.resize(n,0);
-        b.resize(n,0);
-        ans.resize(n,0);
+	if (!padded) {
+		while (n&(n-1)) n += (n&-n); // geq power of 2
+		n += n;
 
-    }
+		a.resize(n,0);
+		b.resize(n,0);
+		ans.resize(n,0);
 
-    vector<int> p(n);
-    p[0] = 0;
-    for (int i = 1; i < n; i++)
-        p[i] = (p[i >> 1] >> 1) | ((i&1) ? (n >> 1) : 0);
+	}
 
-    fft(ans, a, p, 0);
-    fft(a, b, p, 0);
-    for (int i = 0; i < n; i++)
-        a[i] *= ans[i];
-    fft(ans, a, p, 1);
+	vector<int> p(n);
+	p[0] = 0;
+	for (int i = 1; i < n; i++)
+		p[i] = (p[i >> 1] >> 1) | ((i&1) ? (n >> 1) : 0);
+
+	fft(ans, a, p, 0);
+	fft(a, b, p, 0);
+	for (int i = 0; i < n; i++)
+		a[i] *= ans[i];
+	fft(ans, a, p, 1);
 }
