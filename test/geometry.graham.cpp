0413e4f14cb5d76485f36ba2d037c3bdc6080e42
Victor Sena Molero <victorsenam@gmail.com>
2017-10-03 18:57:21 -0300

    Refactor test names for geometry_graham

--- test/geometry.graham.cpp
+++ test/geometry.graham.cpp
@@ -10,7 +10,7 @@ bool operator== (vec a, vec b)
 const vec sample_pointset[] = { vec(7,7), vec(8,7), vec(8,8), vec(10,7), vec(10,8), vec(11,7), vec(11,11), vec(11,9), vec(12,12), vec(13,9), vec(14,10), vec(14,9), vec(14,14), vec(15,14), vec(15,12), vec(15,11) };
 const int sample_size = 16;
 
-TEST(Graham, Simple) {
+TEST(geometry, graham) { 
 	vec *v = new vec[16];
 	vec expect[] = { vec(7,7), vec(11,7), vec(14,9), vec(15,11), vec(15,14), vec(14,14) };
 
@@ -22,14 +22,14 @@ TEST(Graham, Simple) {
 		EXPECT_EQ(v[i], expect[i]) << "Position " << i << " of the hull should match";
 }
 
-TEST(Graham, Degenerate) {
+TEST(geometry, graham__Degenerate) {
 	vec v[] = { vec(0,0), vec(0,0) };
 	
 	EXPECT_EQ(graham(v, 2, 0), 1) << "Repeated points should be irrelevant";
 	EXPECT_EQ(v[0], vec(0,0)) << "Convex hull should be the only point";
 }
 
-TEST(Graham, WithBorder) {
+TEST(geometry, graham__Border) {
 	vec *v = new vec[16];
 	vec expect[] = { vec(7,7), vec(8,7), vec(10,7), vec(11,7), vec(14,9), vec(15,11), vec(15,12), vec(15,14), vec(14,14), vec(12,12), vec(11,11), vec(8,8) };
 
