d9d995ee694fdbae8960030260cd6ad4beb3ab47
Yan Couto <yancouto@gmail.com>
2018-02-05 16:36:26 -0200

    Improved dinic

--- code/dinic.cpp
+++ code/dinic.cpp
@@ -1,15 +1,16 @@
 //typedef int num;
 //const int N = ;
 //const int M =  * 2;
+//const num eps = 0;
 
 struct dinic {
 	int hd[N], seen[N], qu[N], lv[N], ei[N], to[M], nx[M];
 	num fl[M], cp[M];
-	int en = 2, vn = N;
+	int en = 2;
 	int tempo = 0;
 
 	// Reset the whole graph
-	void reset(int n=N) { vn = n; en = 2; memset(hd, 0, sizeof(int) * vn); }
+	void reset(int n=N) { en = 2; memset(hd, 0, sizeof(int) * n); }
 	// Reset only the flow
 	void reset_flow() { memset(fl, 0, sizeof(num) * en); }
 
@@ -20,16 +21,18 @@ struct dinic {
 	}
 
 	bool bfs(int s, int t) {
-		memset(lv, -1, sizeof(int) * vn);
-		lv[s] = 0;
+		seen[t] = ++tempo;
+		lv[t] = 0;
 		int ql = 0, qr = 0;
-		qu[qr++] = s;
+		qu[qr++] = t;
 		while(ql != qr) {
-			s = qu[ql++];
+			t = qu[ql++];
+			ei[t] = hd[t];
 			if(s == t) return true;
-			for(int e = hd[s]; e; e = nx[e])
-				if(lv[to[e]] == -1 && cp[e] - fl[e] > 0) {
-					lv[to[e]] = lv[s] + 1;
+			for(int e = hd[t]; e; e = nx[e])
+				if(seen[to[e]] != tempo && cp[e ^ 1] - fl[e ^ 1] > 0) {
+					seen[to[e]] = tempo;
+					lv[to[e]] = lv[t] + 1;
 					qu[qr++] = to[e];
 				}
 		}
@@ -38,9 +41,8 @@ struct dinic {
 
 	num dfs(int s, int t, num f) {
 		if(s == t) return f;
-		seen[s] = tempo;
 		for(int &e = ei[s]; e; e = nx[e])
-			if(seen[to[e]] != tempo && cp[e] - fl[e] > 0 && lv[to[e]] == lv[s] + 1)
+			if(ei[to[e]] && cp[e] - fl[e] > 0 && lv[to[e]] == lv[s] - 1)
 				if(num rf = dfs(to[e], t, min(f, cp[e] - fl[e]))) {
 					fl[e] += rf;
 					fl[e ^ 1] -= rf;
@@ -51,14 +53,9 @@ struct dinic {
 
 	num max_flow(int s, int t) {
 		num fl = 0;
-		while(bfs(s, t)) {
-			tempo++;
-			for(int i = 0; i < vn; i++) ei[i] = hd[i];
-			while(num f = dfs(s, t, numeric_limits<num>::max())) {
+		while(bfs(s, t))
+			while(num f = dfs(s, t, numeric_limits<num>::max()))
 				fl += f;
-				tempo++;
-			}
-		}
 		return fl;
 	}
 };
