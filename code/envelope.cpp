b8fad60018ccda081599623150a3a8205c486b05
Victor Sena Molero <victorsenam@gmail.com>
2018-03-01 19:30:02 -0300

    New envelope code and tests
    solves #19

--- code/envelope.cpp
+++ code/envelope.cpp
@@ -1,69 +1,63 @@
-// typedef ll num; const num eps = 0;
-// XXX double warnings means there's some specific integer operation (+1, lo<hi, etc...), not precision-related
-struct line {
-	num a, b;
-	num operator () (num x) const { return a*x + b; }
-	bool operator < (const line & ot) const { return a > ot.a; } // decreasing slope, if function ties, the smallest in this order is chosen
+// typedef ll num; const num eps = 0; num INF = 1e17;
+// XXX double: indicates operations specific to integers (+1,lo<hi,...), non precision-related
+template<typename line> struct envelope { // if line is used, the template tag can be simply removed
+	deque<line> q; num lo,hi; envelope (num _lo, num _hi) : lo(_lo), hi(_hi) {}
+	void push_front (line l) { assert(q.empty() || !(q.front()<l)); // l is best at lo or never
+		if (!q.empty() && q.front()(lo) < l(lo)) return;
+		for (num x; q.size(); q.pop_front()) {
+			x = (q.size()<=1?hi:min(max(lo,q.front().inter(*next(q.begin()))-1),hi+1));
+			if (l(x) > q.front()(x)) break;
+		}
+		q.push_front(l);
+	}
+	void push_back (line l) { assert(q.empty() || !(l<q.back())); // l is best at hi or never
+		if (!q.empty() && q.back()(hi) <= l(hi)) return;
+		for (num x; q.size(); q.pop_back()) {
+			x = (q.size()<=1?lo:min(max(lo,next(q.rbegin())->inter(q.back())),hi+1));
+			if (l(x) >= q.back()(x)) break;
+		}
+		q.push_back(l);
+	}
+	void pop_front (num _lo) { assert(lo<=_lo+eps); for(lo=_lo;q.size()>1&&q.front()(lo)>(*next(q.begin()))(lo);q.pop_front()); } // always amort. O(1)
+	void pop_back (num _hi) { assert(hi>=_hi-eps); for(hi=_hi;q.size()>1&&(*next(q.rbegin()))(hi)<=q.back()(hi);q.pop_back()); } // always amort. O(1)
+	line get (num x) { return (*lower_bound(q.begin(),q.end()-1,q.front(),[x](line & a, const line & o){ return a(x)>(*((&a)+1))(x); })); } // always O(lg(n))
 };
-struct envelope { // min envelope, the first element is the best at the beggining
-	deque<line> qu; num lo, hi; envelope (num a, num b) : lo(a), hi(b) {}
-	num inter (line a, line b) { assert(!(b<a)); // first point where b beats (properly) a XXX double
-		if (b.a == a.a) return (a.b < b.b)?hi+1:lo;
-		return max(min((b.b - a.b - (b.b - a.b<0)*(a.a - b.a - 1))/(a.a - b.a) + 1,hi+1),lo);
-		num lo = this->lo; num hi = this->hi + 1;
+struct line { // env operations in amort. O(1)
+	num a,b; num operator () (num x) const { return a*x+b; }
+	bool operator < (const line & ot) const { return a>ot.a; } // decreasing slope, if function ties, envelope gets minimum here
+	num inter (line o) { assert(!(o<(*this))); return (o.a==a)?((b<o.b)?INF:-INF):(o.b-b-(o.b-b<0)*(a-o.a-1))/(a-o.a) + 1; }
+};
+struct generic_line { // change only parameters and () for any viable functions, env operations in amort. O(lg(hi-lo))
+	envelope<generic_line> & env; num a,b; num operator () (num x) const { return a*x+b; }
+	bool operator < (const generic_line & ot) const { return (*this)(env.lo) < ot(env.lo) && (*this)(env.hi) > ot(env.hi); } // first element is best at lo
+	num inter (generic_line o) { assert(!(o<(*this)));
+		num lo = env.lo; num hi = env.hi+1;
 		while (lo < hi) {
-			num md = lo + (hi - lo)/2;
-			if (a(md) <= b(md)) lo = md+1;
+			num md = lo+(hi-lo)/2;
+			if ((*this)(md)<=o(md)) lo = md+1;
 			else hi = md;
 		}
 		return lo;
 	}
-	void push_back (line l) { assert(qu.empty() || !(l < qu.back())); // insert a line that is best at hi or never
-		if (!qu.empty() && qu.back()(hi) <= l(hi)) return;
-		for (num x; qu.size(); qu.pop_back()) {
-			x = (qu.size()<=1?lo:inter(qu[qu.size()-2],qu.back()));
-			if (l(x) >= qu.back()(x)) break;
-		}
-		qu.push_back(l);
-	}
-	void push_front (line l) { assert(qu.empty() || !(qu.front() < l)); // insert a line that is best at lo or never
-		if (!qu.empty() && qu.front()(lo) < l(lo)) return;
-		for (num x; qu.size(); qu.pop_front()) {
-			x = qu.size()<=1?hi:inter(qu[0],qu[1]);
-			if (l(x) > qu.front()(x)) break;
-		}
-		qu.push_front(l);
-	}
-	line pop_front (num lo) { assert(!qu.empty() && this->lo <= lo + eps); // gets best line at this new lo
-		for (this->lo = lo; qu.size() > 1 && qu[0](lo) > qu[1](lo); qu.pop_front());
-		return qu.front();
-	}
-	line pop_back (num hi) { assert(!qu.empty() && this->hi >= hi - eps); // gets best line at this new hi
-		for (this->hi = hi; qu.size() > 1 && qu[qu.size()-2](hi) <= qu.back()(hi); qu.pop_back());
-		return qu.back();
-	}
-	line get (num x) { assert(!qu.empty() && lo <= x + eps && x <= hi + eps); // gets best line at x
-		return (*lower_bound(qu.begin(),qu.end()-1,qu[0],[x](line & a, const line & o){ return a(x) > (*((&a)+1))(x); }));
-	}
 };
-struct full_envelope {
-	vector<envelope> v; full_envelope(envelope c) : v({c}) {}
-	void add (line l) {
-		envelope nw(v[0].lo,v[0].hi); nw.push_back(l);
-		while (v.size() && v.back().qu.size() <= nw.qu.size()) {
-			envelope aux(nw.lo,nw.hi); auto jt = nw.qu.begin();
-			for (line r : v.back().qu) {
-				while (jt != nw.qu.end() && *jt < r) aux.push_back(*(jt++));
+template<typename line> struct full_envelope {
+	vector<envelope<line> > v; full_envelope(envelope<line> c) : v({c}) {}
+	void add (line l) { // amort. O(lg(n)*inter)
+		envelope<line> nw(v[0].lo,v[0].hi); nw.push_back(l);
+		while (v.size() && v.back().q.size() <= nw.q.size()) {
+			envelope<line> aux(nw.lo,nw.hi); auto jt = nw.q.begin();
+			for (line r : v.back().q) {
+				while (jt != nw.q.end() && *jt < r) aux.push_back(*(jt++));
 				aux.push_back(r);
 			}
-			while (jt != nw.qu.end()) aux.push_back(*(jt++));
+			while (jt != nw.q.end()) aux.push_back(*(jt++));
 			nw = aux; v.pop_back();
 		}
 		v.push_back(nw);
 	}
-	line get (num x) { // you can easily use pop_front and pop_back here
+	line get (num x) { // O(lg(n)^2) use pop_back/pop_front for better time
 		line b = v.front().get(x);
-		for (int i = 1; i < v.size(); i++) {
+		for (int i = 1; i < (int) v.size(); i++) {
 			line c = v[i].get(x);
 			if (c(x)!=b(x)?c(x)<b(x):c<b) b = c;
 		}
--- test/envelope.ll.cpp
+++ test/envelope.ll.cpp
@@ -6,7 +6,7 @@ typedef long long ll;
 typedef pair<ll,ll> pii;
 #define pb push_back
 
-typedef ll num; const num eps = 0;
+typedef ll num; const num eps = 0; num INF = 1e17;
 
 #include "gtest/gtest.h"
 #include "../code/envelope.cpp"
@@ -15,152 +15,183 @@ bool operator== (line a, line b) {
 	return a.a == b.a && a.b == b.b;
 }
 
-bool operator== (const envelope & a, const envelope & b) {
-	return a.qu == b.qu && a.lo == b.lo && a.hi == b.hi;
+template<typename line> bool operator== (const envelope<line> & a, const envelope<line> & b) {
+	return a.lo == b.lo && a.hi == b.hi && a.q == b.q;
 }
 
-ostream & operator<< (ostream & os, line a) {
-	return os << "(" << a.a << "x + " << a.b << ")";
-}
+ostream & operator<< (ostream & os, line a) { return os << "(" << a.a << "x + " << a.b << ")"; }
+ostream & operator<< (ostream & os, generic_line a) { return os << "(" << a.a << "x + " << a.b << ")"; }
 
-ostream & operator<< (ostream & os, envelope e) {
+template<typename line> ostream & operator<< (ostream & os, envelope<line> e) {
 	os << "(";
-	for (unsigned int i = 0; i < e.qu.size(); i++) {
+	for (unsigned int i = 0; i < e.q.size(); i++) {
 		if (i)
 			os << " ";
-		os << e.qu[i];
+		os << e.q[i];
 	}
 	return os << ")[" << e.lo << ".." << e.hi << "]";
 }
 
-TEST(EnvelopeInter, Simple) {
-	envelope env(-1e9,1e9);
+TEST(line, inter) {
+	EXPECT_EQ(line({7,3}).inter({2,14}), 3) << "Should return floor of line intersection + 1 on simple cases.";
+	EXPECT_EQ(line({7,14}).inter({2,3}), -2) << "Should return floor of line intersection + 1 on simple cases.";
+	EXPECT_EQ(line({8,2}).inter({4,0}), 0) << "Should return floor of line intersection + 1 on simple cases.";
+	EXPECT_EQ(line({8,0}).inter({4,0}), 1) << "Should return floor of line intersection + 1 on simple cases.";
+	EXPECT_EQ(line({8,16}).inter({4,-16}), -7) << "Should return floor of line intersection + 1 on simple cases.";
+	EXPECT_EQ(line({8,-16}).inter({4,16}), 9) << "Should return floor of line intersection + 1 on simple cases.";
+	EXPECT_EQ(line({4213,321}).inter({324,2442}), 1) << "Should return floor of line intersection + 1 on simple cases.";
 
-	EXPECT_EQ(env.inter({7,3},{2,14}), 3) << "Should return ceil of line intersection on simple cases.";
-	EXPECT_EQ(env.inter({7,14},{2,3}), -2) << "Should return ceil of line intersection on simple cases.";
-	EXPECT_EQ(env.inter({4213,321},{324,2442}), 1) << "Should return ceil of line intersection on simple cases.";
+	EXPECT_EQ(line({1,3}).inter({1,2}), -INF) << "Should return -INF with parallels where b beats a.";
+	EXPECT_EQ(line({1,2}).inter({1,3}), INF) << "Should return INF with parallels where a beats b.";
 }
 
-TEST(EnvelopeInter, Parallels) {
-	envelope env(-1e9,1e9);
+TEST(generic_line, inter) {
+	envelope<generic_line> env(-10,10);
 
-	EXPECT_EQ(env.inter({1,3},{1,2}), env.lo) << "Should return lo with parallels where b beats a.";
-	EXPECT_EQ(env.inter({1,2},{1,3}), env.hi+1) << "Should return hi+1 with parallels where a beats b.";
+	EXPECT_EQ(generic_line({env,7,3}).inter({env,2,14}), 3) << "Should return floor of line intersection + 1 on simple cases.";
+	EXPECT_EQ(generic_line({env,7,14}).inter({env,2,3}), -2) << "Should return floor of line intersection + 1 on simple cases.";
+	EXPECT_EQ(generic_line({env,8,2}).inter({env,4,0}), 0) << "Should return floor of line intersection + 1 on simple cases.";
+	EXPECT_EQ(generic_line({env,8,0}).inter({env,4,0}), 1) << "Should return floor of line intersection + 1 on simple cases.";
+	EXPECT_EQ(generic_line({env,8,16}).inter({env,4,-16}), -7) << "Should return floor of line intersection + 1 on simple cases.";
+	EXPECT_EQ(generic_line({env,8,-16}).inter({env,4,16}), 9) << "Should return floor of line intersection + 1 on simple cases.";
+
+	EXPECT_EQ(generic_line({env,1,3}).inter({env,1,2}), env.lo) << "Should return env.lo with parallels where b beats a.";
+	EXPECT_EQ(generic_line({env,1,2}).inter({env,1,3}), env.hi+1) << "Should return env.hi+1 with parallels where a beats b.";
+
+	EXPECT_EQ(generic_line({env,2,10000}).inter({env,-2,0}), env.lo) << "Should return lo when b beats a everywhere in range.";
+	EXPECT_EQ(generic_line({env,2,0}).inter({env,-2,1000}), env.hi+1) << "Should return hi+1 when a beats b everywhere in range.";
 }
 
-TEST(EnvelopeInter, LowRange) {
-	envelope env(-10,10);
-
-	EXPECT_EQ(env.inter({2,10000},{-2,0}), env.lo) << "Should return lo when b beats a everywhere in range.";
-	EXPECT_EQ(env.inter({2,0},{-2,1000}), env.hi+1) << "Should return hi+1 when a beats b everywhere in range.";
-}
-
-TEST(Envelope, Simple) {
-	envelope env[2] = { envelope(-100,100), envelope(-100,100) };
+TEST(envelope, Behaviour) {
+	envelope<line> env[2] = { envelope<line>(-100,100), envelope<line>(-100,100) };
 
 	// basic
 	env[0].push_back({10,5});
-	EXPECT_EQ(env[0].qu.size(), 1) << "push_back should add first line.";
+	EXPECT_EQ(env[0].q.size(), 1) << "push_back should add first line.";
 	env[1].push_front({9,-3});
-	EXPECT_EQ(env[1].qu.size(), 1) << "push_front should add first line.";
+	EXPECT_EQ(env[1].q.size(), 1) << "push_front should add first line.";
 
 	env[0].push_back({9,-3});
+	EXPECT_EQ(env[0].q.size(), 2) << "push_back should add intersecting line.";
+	EXPECT_EQ(env[0].q.back(), line({9,-3})) << "push_back should add to the back.";
 	env[1].push_front({10,5});
-	EXPECT_EQ(env[0].qu.size(), 2) << "push_back should add intersecting line.";
-	EXPECT_EQ(env[0].qu.back(), line({9,-3})) << "push_back should add to the back.";
-	EXPECT_EQ(env[1].qu.size(), 2) << "push_front should add intersecting line.";
-	EXPECT_EQ(env[0].qu.front(), line({10,5})) << "push_front should add to the front.";
+	EXPECT_EQ(env[1].q.size(), 2) << "push_front should add intersecting line.";
+	EXPECT_EQ(env[1].q.front(), line({10,5})) << "push_front should add to the front.";
 	EXPECT_EQ(env[0], env[1]) << "at this point operations made should be equivalent.";
 
-
-	EXPECT_EQ(env[0].pop_back(100), line({9,-3})) << "pop_back should answer min envelope value.";
-	EXPECT_EQ(env[0].pop_front(-100), line({10,5})) << "pop_front should answer min envelope value.";
+	env[0].pop_back(100);
+	EXPECT_EQ(env[0].q.back(), line({9,-3})) << "pop_back should answer min envelope value.";
 	EXPECT_EQ(env[0].hi, 100) << "pop_back on hi should not change hi.";
+	env[0].pop_front(-100);
+	EXPECT_EQ(env[0].q.front(), line({10,5})) << "pop_front should answer min envelope value.";
 	EXPECT_EQ(env[0].lo, -100) << "pop_front on lo should not change lo.";
 	EXPECT_EQ(env[0], env[1]) << "trivial pops should not alter deque.";
 
 	env[0].push_back({7,-4});
-	EXPECT_EQ(env[0].qu.size(), 3) << "push_back should add intersecting line.";
-	EXPECT_EQ(env[0].qu.back(), line({7,-4})) << "push_back should add to the back.";
+	EXPECT_EQ(env[0].q.size(), 3) << "push_back should add intersecting line.";
+	EXPECT_EQ(env[0].q.back(), line({7,-4})) << "push_back should add to the back.";
 
 	// parallels
 	env[0].push_back({7,-3});
-	EXPECT_EQ(env[0].qu.size(), 3) << "push_back should ignore some parallel line.";
-	EXPECT_EQ(env[0].qu.back(), line({7,-4})) << "push_back should keep best parallel line.";
+	EXPECT_EQ(env[0].q.size(), 3) << "push_back should ignore some parallel line.";
+	EXPECT_EQ(env[0].q.back(), line({7,-4})) << "push_back should keep best parallel line.";
 
 	env[0].push_back({7,-5});
-	EXPECT_EQ(env[0].qu.size(), 3) << "push_back should ignore some parallel line.";
-	EXPECT_EQ(env[0].qu.back(), line({7,-5})) << "push_back should remove worst parallel line.";
+	EXPECT_EQ(env[0].q.size(), 3) << "push_back should ignore some parallel line.";
+	EXPECT_EQ(env[0].q.back(), line({7,-5})) << "push_back should remove worst parallel line.";
 
 	env[1].push_back({7,-5});
-	EXPECT_EQ(env[0], env[1]) << "mixing push_back and push_front should be ok, adding parallels should be irrelevant.";
+	ASSERT_EQ(env[0], env[1]) << "old useless parallels are irrelevant.";
 
 	env[1].push_front({10, 4});
-	EXPECT_EQ(env[1].qu.size(), 3) << "push_front should ignore some parallel line.";
-	EXPECT_EQ(env[1].qu.front(), line({10,4})) << "push_front should remove worst parallel line.";
+	EXPECT_EQ(env[1].q.size(), 3) << "push_front should ignore some parallel line.";
+	EXPECT_EQ(env[1].q.front(), line({10,4})) << "push_front should remove worst parallel line.";
 
 	env[1].push_front({10, 6});
-	EXPECT_EQ(env[1].qu.size(), 3) << "push_front should ignore some parallel line.";
-	EXPECT_EQ(env[1].qu.front(), line({10,4})) << "push_front should keep best parallel line.";
+	EXPECT_EQ(env[1].q.size(), 3) << "push_front should ignore some parallel line.";
+	EXPECT_EQ(env[1].q.front(), line({10,4})) << "push_front should keep best parallel line.";
 
-	env[0].push_front({10,4});
+	env[0].push_front({10, 4});
 	EXPECT_EQ(env[0], env[1]) << "mixing push_back and push_front should be ok, adding parallels should be irrelevant.";
 
 	// useless
 	env[0].push_back({0,9});
-	EXPECT_EQ(env[0].qu.size(), 4) << "push_back should add another line.";
+	EXPECT_EQ(env[0].q.size(), 4) << "push_back should add another line.";
 
 	env[0].push_back({-1,10});
-	EXPECT_EQ(env[0].qu.size(), 4) << "push_back should remove useless line.";
-	EXPECT_EQ(env[0].qu.back(), line({-1,10})) << "push_back should remove useless line.";
+	EXPECT_EQ(env[0].q.size(), 4) << "push_back should remove useless line.";
+	EXPECT_EQ(env[0].q.back(), line({-1,10})) << "push_back should remove useless line.";
 
 	env[1].push_back({-1,10});
 	EXPECT_EQ(env[0], env[1]) << "adding useless lines should be irrelevant.";
 
 	// out of bounds
 	env[0].push_back({-2,10000});
-	EXPECT_EQ(env[0].qu.size(), 4) << "lines only useful out of bounds should be ignored.";
+	EXPECT_EQ(env[0].q.size(), 4) << "lines only useful out of bounds should be ignored.";
 	EXPECT_EQ(env[0], env[1]) << "lines only useful out of bounds should be ignored.";
 	env[0].push_front({90,11000});
-	EXPECT_EQ(env[0].qu.size(), 4) << "lines only useful out of bounds should be ignored.";
+	EXPECT_EQ(env[0].q.size(), 4) << "lines only useful out of bounds should be ignored.";
 	EXPECT_EQ(env[0], env[1]) << "lines only useful out of bounds should be ignored.";
 
 	// pops
-	EXPECT_EQ(env[0].pop_back(1), line({7,-5})) << "pop_back should return correct line.";
+	env[0].pop_back(1);
+	EXPECT_EQ(env[0].q.back(), line({7,-5})) << "pop_back should return correct line.";
 	EXPECT_EQ(env[0].hi, 1) << "pop_back should move bound.";
-	EXPECT_EQ(env[0].qu.size(), 3) << "pop_back should remove useless lines.";
+	EXPECT_EQ(env[0].q.size(), 3) << "pop_back should remove useless lines.";
 
-	EXPECT_EQ(env[1].pop_front(0), line({7,-5})) << "pop_front should return correct line.";
+	env[1].pop_front(0);
+	EXPECT_EQ(env[1].q.front(), line({7,-5})) << "pop_front should return correct line.";
 	EXPECT_EQ(env[1].lo, 0) << "pop_front should move bound.";
-	EXPECT_EQ(env[1].qu.size(), 2) << "pop_front should remove useless lines.";
+	EXPECT_EQ(env[1].q.size(), 2) << "pop_front should remove useless lines.";
 
-	EXPECT_EQ(env[0].pop_front(-90), line({10,4})) << "pop_front should return correct line.";
+	env[0].pop_front(-90);
+	EXPECT_EQ(env[0].q.front(), line({10,4})) << "pop_front should return correct line.";
 	EXPECT_EQ(env[0].lo, -90) << "pop_front should move bound.";
-	EXPECT_EQ(env[0].qu.size(), 3) << "pop_front should not remove lines unless necessary.";
+	EXPECT_EQ(env[0].q.size(), 3) << "pop_front should not remove lines unless necessary.";
 
-	EXPECT_EQ(env[0].pop_front(0), line({7,-5})) << "mixing pop_front and pop_back should be ok.";
+	env[0].pop_front(0);
+	EXPECT_EQ(env[0].q.front(), line({7,-5})) << "mixing pop_front and pop_back should be ok.";
 	EXPECT_EQ(env[0].lo, 0) << "pop_front should move bound.";
 
-	EXPECT_EQ(env[1].pop_back(1), line({7,-5})) << "mixing pop_front and pop_back should be ok.";
+	env[1].pop_back(1);
+	EXPECT_EQ(env[1].q.back(), line({7,-5})) << "mixing pop_front and pop_back should be ok.";
 	EXPECT_EQ(env[1].hi, 1) << "pop_back should move bound.";
 
 	EXPECT_EQ(env[0], env[1]) << "mixing pop_front and pop_back should be ok.";
 
 	env[0].push_front({7,-6});
-	EXPECT_EQ(env[0].qu.size(), 1) << "push_front should accept globally best.";
+	EXPECT_EQ(env[0].q.size(), 1) << "push_front should accept globally best.";
 	env[1].push_back({7,-6});
-	EXPECT_EQ(env[1].qu.size(), 1) << "push_back should accept globally best.";
+	EXPECT_EQ(env[1].q.size(), 1) << "push_back should accept globally best.";
 
 	EXPECT_EQ(env[0],env[1]) << "push_front and push_back with globally best and same slope should result in equality.";
 }
 
-TEST(FullEnvelope, Random) {
-	srand(time(NULL)); rand(); rand();
+TEST(line, InterRandom) {
+	unsigned int x = time(NULL);
+	cout << "Seed " << x << endl;
+	srand(x); rand(); rand();
+	int ts = 1e6, p_lim = 1e8;
+	while (ts--) {
+		envelope<generic_line> env(-p_lim + (rand()%200),p_lim - (rand()%200));
+		line a({(rand()%(p_lim+p_lim+1)) - p_lim, (rand()%(p_lim+p_lim+1)) - p_lim});
+		line b({(rand()%(p_lim+p_lim+1)) - p_lim, (rand()%(p_lim+p_lim+1)) - p_lim});
+		if (b < a) swap(a,b);
+		generic_line ga({env,a.a,a.b});
+		generic_line gb({env,b.a,b.b});
+		EXPECT_EQ(min(max(env.lo,a.inter(b)),env.hi+1), ga.inter(gb));
+	};
+}
+
+TEST(full_envelope, Random) {
+	unsigned int x = time(NULL);
+	cout << "Seed " << x << endl;
+	srand(x); rand(); rand();
 	int ts = 50, p_lim = 1e5;
 	while (ts--) {
 		int lo = -p_lim + (rand()%200);
 		int hi = p_lim - (rand()%200);
-		full_envelope ev(envelope(lo,hi));
+		full_envelope<line> ev(envelope<line>(lo,hi));
 		int n = 900 + (rand()%200);
 		vector<line> v;
 
