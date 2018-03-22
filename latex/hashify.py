ef4dc199158fec59b8b89f87a9c11f6f07c762c5
Victor Sena Molero <victorsenam@gmail.com>
2018-03-22 13:25:11 -0300

    Reset feature on hashify

--- latex/hashify.py
+++ latex/hashify.py
@@ -1,11 +1,13 @@
-import hashlib,sys
+import hashlib,sys,string
 
 m = hashlib.md5()
 for line in sys.stdin.readlines():
 	safe = line
+	line = "".join(line.split())
+	if line.endswith("$"):
+		m = hashlib.md5()
 	if line.find("//") != -1:
 		line = line[:line.find("//")]
-	trimmed = line.replace(" ","").replace("\n","").replace("\t","")
-	m.update(trimmed.encode('utf-8'))
+	m.update(line.encode('utf-8'))
 	hash = m.hexdigest()[:4]
 	print("%s %s"%(hash,safe), end='')
