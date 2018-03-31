7d685998ed5d0a7dc9ffd2fc673d07146df27f6a
Victor Sena Molero <victorsenam@gmail.com>
2018-03-31 16:16:16 -0300

    Adding hash resets on basic geometry

--- code/geometry/basic.cpp
+++ code/geometry/basic.cpp
@@ -16,11 +16,14 @@ struct vec {
 	inline int ccw (vec a, vec b) { cood o = cross(a,b); return (eps < o) - (o < -eps); } // this is to the (1 left, 0 over, -1 right) of ab
 	inline int dir (vec a, vec b) { cood o = inner(a,b); return (eps < o) - (o < -eps); } // a(this) is to the (1 same, 0 none, -1 opposite) direction of ab
 	inline cood sq (vec o = vec()) { return inner(o,o); }
-	inline double nr (vec o = vec()) { return sqrt(sq(o)); }
+	inline double nr (vec o = vec()) { return sqrt(sq(o)); } //$
 	inline vec operator ~ () { return (*this)/nr(); }
 	inline vec proj (vec a, vec b) { return a + (b-a)*(a.inner((*this),b) / a.sq(b)); } // projects this onto line ab
 	inline vec rotate (double a) { return vec(cos(a) * x - sin(a) * y, sin(a) * x + cos(a) * y); } // ccw by a radians
-	inline vec rot90 () { return vec(-y,x); } // rotate(pi/2)
+	inline vec rot90 () { return vec(-y,x); } // rotate(pi/2)$
+	bool in_seg (vec a, vec b) { return ccw(a,b) == 0 && dir(a,b) <= 0; } // tips included
+	double dist2_lin (vec a, vec b) { return a.sq(b) <= eps ? sq(a) : double(::sq(cross(a,b)))/a.sq(b); } // see cir.has_inter_lin
+	double dist2_seg (vec a, vec b) { return a.dir((*this),b) == (b.dir((*this),a)) ? dist2_lin(a,b) : min(sq(a),sq(b)); }
 	inline bool operator == (const vec & o) const { return abs(x-o.x) <= eps && abs(y-o.y) <= eps; }
 	inline bool operator < (const vec & o) const { return (abs(x-o.x)>eps)?(x < o.x):(y > o.y); } // lex compare (inc x, dec y)
 	// full ccw angle strict compare beginning upwards (this+(0,1)) around (*this)
@@ -29,10 +32,7 @@ struct vec {
 		if ((*this < a) != (*this < b)) return *this < b;
 		int o = ccw(a,b); return o?o>0:((a == *this && !(a == b)) || a.dir(*this,b) < 0);
 	}
-	bool in_seg (vec a, vec b) { return ccw(a,b) == 0 && dir(a,b) <= 0; } // tips included
-	double dist2_lin (vec a, vec b) { return a.sq(b) <= eps ? sq(a) : double(::sq(cross(a,b)))/a.sq(b); } // see cir.has_inter_lin
-	double dist2_seg (vec a, vec b) { return a.dir((*this),b) == (b.dir((*this),a)) ? dist2_lin(a,b) : min(sq(a),sq(b)); }
-};
+}; //$
 struct lin { // line
 	vec p; cood c; // p*(x,y) = c
 	lin () {} lin (vec a, cood b) : p(a), c(b) {}
@@ -44,8 +44,7 @@ struct lin { // line
 	vec at_x (cood x) { return vec(x,(c-p.x*x)/p.y); }
 	vec at_y (cood y) { return vec((c-y*p.y)/p.x,y); }
 	double sign_dist (vec v) { return double(p*v - c)/p.nr(); }
-};
-ostream& operator<<(ostream& os, vec o) { return os << '(' << o.x << ", " << o.y << ')'; }
+}; //$
 struct cir { // circle
 	vec c; cood r;
 	cir () {} cir (vec v, cood d) : c(v), r(d) {}
@@ -92,11 +91,12 @@ struct cir { // circle
 		if (contains(a)) return c.cross(a,itr.first)/2 + arc_area(itr.first,b);
 		return arc_area(a,itr.second) + c.cross(itr.second,itr.first)/2 + arc_area(itr.first,b);
 	}
-};
+}; //$
 bool inter_seg (vec a, vec b, vec c, vec d) {
 	if (a.in_seg(c, d) || b.in_seg(c, d) || c.in_seg(a, b) || d.in_seg(a, b)) return true;
 	return (c.ccw(a, b) * d.ccw(a, b) == -1 && a.ccw(c, d) * b.ccw(c, d) == -1);
 }
 double dist2_seg (vec a, vec b, vec c, vec d){return inter_seg(a,b,c,d)?0.:min({ a.dist2_seg(c,d), b.dist2_seg(c,d), c.dist2_seg(a,b), d.dist2_seg(a,b) });}
+ostream& operator<<(ostream& os, vec o) { return os << '(' << o.x << ", " << o.y << ')'; }
 ostream& operator<<(ostream& os, lin o) { return os << '[' << o.p << "*(x,y) = " << o.c << ']'; }
 ostream& operator<<(ostream& os, cir o) { return os << '[' << o.c << o.r << ']'; }
