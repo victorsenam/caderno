df3b29ae52b1a801cd59ea073bb56a7540962604
Victor Sena Molero <victorsenam@gmail.com>
2018-04-02 13:39:13 -0300

    Reformatting dinic

--- code/dinic.cpp
+++ code/dinic.cpp
@@ -1,61 +1,37 @@
-//typedef int num;
-//const int N = ;
-//const int M =  * 2;
-//const num eps = 0;
-
+//typedef int num; const int N = ; const int M =  * 2; const num eps = 0;
 struct dinic {
-	int hd[N], seen[N], qu[N], lv[N], ei[N], to[M], nx[M];
-	num fl[M], cp[M];
-	int en = 2;
-	int tempo = 0;
-
-	// Reset the whole graph
-	void reset(int n=N) { en = 2; memset(hd, 0, sizeof(int) * n); }
-	// Reset only the flow
+	int hd[N], seen[N], qu[N], lv[N], ei[N], to[M], nx[M]; num fl[M], cp[M]; int en = 2; int tempo = 0;
+	void reset(int n=N) { en = 2; memset(hd, 0, sizeof(int) * n); } // resets all
 	void reset_flow() { memset(fl, 0, sizeof(num) * en); }
-
-	// edge from a to b with cap c and edge from b to a with cap rc
 	void add_edge(int a, int b, num c, num rc=0) {
 		to[en] = b; nx[en] = hd[a]; fl[en] = 0; cp[en] = c; hd[a] = en++;
 		to[en] = a; nx[en] = hd[b]; fl[en] = 0; cp[en] = rc; hd[b] = en++;
 	}
-
 	bool bfs(int s, int t) {
-		seen[t] = ++tempo;
-		lv[t] = 0;
-		int ql = 0, qr = 0;
-		qu[qr++] = t;
+		seen[t] = ++tempo; lv[t] = 0; int ql = 0, qr = 0; qu[qr++] = t;
 		while(ql != qr) {
-			t = qu[ql++];
-			ei[t] = hd[t];
-			if(s == t) return true;
-			for(int e = hd[t]; e; e = nx[e])
-				if(seen[to[e]] != tempo && cp[e ^ 1] - fl[e ^ 1] > eps) {
-					seen[to[e]] = tempo;
-					lv[to[e]] = lv[t] + 1;
-					qu[qr++] = to[e];
-				}
+			t = qu[ql++]; ei[t] = hd[t]; if(s == t) return true;
+			for(int e = hd[t]; e; e = nx[e]) if(seen[to[e]] != tempo && cp[e ^ 1] - fl[e ^ 1] > eps) {
+				seen[to[e]] = tempo;
+				lv[to[e]] = lv[t] + 1;
+				qu[qr++] = to[e];
+			}
 		}
 		return false;
 	}
-
 	num dfs(int s, int t, num f) {
 		if(s == t) return f;
-		for(int &e = ei[s]; e; e = nx[e])
-			if(ei[to[e]] && seen[to[e]] == tempo && cp[e] - fl[e] > eps && lv[to[e]] == lv[s] - 1)
-				if(num rf = dfs(to[e], t, min(f, cp[e] - fl[e]))) {
-					fl[e] += rf;
-					fl[e ^ 1] -= rf;
-					return rf;
-				}
+		for(int &e = ei[s]; e; e = nx[e]) if(ei[to[e]] && seen[to[e]] == tempo && cp[e] - fl[e] > eps && lv[to[e]] == lv[s] - 1)
+			if(num rf = dfs(to[e], t, min(f, cp[e] - fl[e]))) {
+				fl[e] += rf;
+				fl[e ^ 1] -= rf;
+				return rf;
+			}
 		return 0;
 	}
-
 	num max_flow(int s, int t) {
 		num fl = 0;
-		while(bfs(s, t))
-			while(num f = dfs(s, t, numeric_limits<num>::max()))
-				fl += f;
+		while (bfs(s, t)) for(num f; (f = dfs(s, t, numeric_limits<num>::max())); fl += f);
 		return fl;
 	}
 };
