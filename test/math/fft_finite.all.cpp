ff1b77baa777c4eaf91cc4ec14d30246e225c4c1
Victor Sena Molero <victorsenam@gmail.com>
2018-04-02 18:11:55 -0300

    FFT finite reformatting

--- test/math/fft_finite.all.cpp
+++ test/math/fft_finite.all.cpp
@@ -1,9 +1,17 @@
 #include "gtest/gtest.h"
+#include <bits/stdc++.h>
+#define cout if (1) cout
+
+using namespace std;
+typedef long long int ll;
+typedef pair<ll,ll> pii;
+#define pb push_back
 const int N = 112345;
-typedef long long num;
+typedef ll num;
 #include "../../code/math/fft_finite.cpp"
 
-num a[N], b[N], c[N];
+num a[N], b[N], c[N], w[N];
+int pr[N];
 
 vector<ll> naive (vector<ll> & a, vector<ll> & b, ll p) {
 	vector<ll> c(a.size() + b.size(), 0);
@@ -22,11 +30,11 @@ TEST(fft_finite, Simple) {
 	i = 0; for (int x : {0,1,0,0}) b[i++] = x;
 	for (i = n/2; i < n; i++) a[i] = b[i] = 0;
 
-	fft_finite(a, c, n, 1, p);
+	fft_finite(a, c, n, 1, p, pr, w);
 	i = 0; for (int x : {5,12,1,11,5,12,1,11}) EXPECT_EQ(c[i++],x);
-	fft_finite(b, a, n, 1, p);
+	fft_finite(b, a, n, 1, p, pr, w);
 	for (int i = 0; i < n; i++) a[i] = (a[i]*c[i])%p;
-	fft_finite(a, b, n, -1, p);
+	fft_finite(a, b, n, -1, p, pr, w);
 	i = 0; for (int x : {0,3,0,2,0,0,0,0}) EXPECT_EQ(b[i++],x);
 }
 
@@ -51,10 +59,10 @@ TEST(fft_finite, SmallRandom) {
 		n += n;
 		assert(c.size() == unsigned(n));
 		a.resize(n,0); b.resize(n,0);
-		fft_finite(a.data(), ::a, n, 1, p);
-		fft_finite(b.data(), ::b, n, 1, p);
+		fft_finite(a.data(), ::a, n, 1, p, pr, w);
+		fft_finite(b.data(), ::b, n, 1, p, pr, w);
 		for (int i = 0; i < n; i++) ::a[i] = (::a[i]*::b[i])%p;
-		fft_finite(::a, ::b, n, -1, p);
+		fft_finite(::a, ::b, n, -1, p, pr, w);
 		
 		for (int i = 0; i < n; i++)
 			EXPECT_EQ(::b[i], c[i]) << i << "-th element differ";
