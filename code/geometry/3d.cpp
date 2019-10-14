cb442030566fb20d5e8c49c53689c3c8e3ccf3a6
Victor Sena Molero <victorsenam@gmail.com>
2019-10-14 15:09:19 +0100

    Revert "Fixed in_tri"

--- code/geometry/3d.cpp
+++ code/geometry/3d.cpp
@@ -20,7 +20,7 @@ struct pnt { // TODO it's not tested at all :)
 	inline pnt operator ~ () { return (*this)/nr(); }
 
 	inline bool in_seg (pnt a, pnt b) { return cross(a,b).sq() <= eps && inner(a,b) <= eps; } // tips included
-	inline bool in_tri (pnt a, pnt b, pnt c) { return abs(mixed(a,b,c)) <= eps && cross(b,c)*cross(b,c) >= -eps && cross(a,b)*cross(c,a) >= -eps; } // border included$
+	inline bool in_tri (pnt a, pnt b, pnt c) { return abs(mixed(a,b,c)) <= eps && cross(a,b)*cross(b,c) >= -eps && cross(a,b)*cross(c,a) >= -eps; } // border included$
 
 	inline pnt proj (pnt a, pnt b) { return a + (b-a)*a.inner(b,(*this))/a.sq(b); }
 	inline pnt proj (pnt a, pnt b, pnt c) { pnt n = a.cross(b,c); return (*this) - n*(n*((*this)-a))/n.sq(); }
